9417853d779d78600b75b38b7bab2eec8f10931b
danielsinai <daniel.sinai.main@gmail.com>
2023-03-30 17:05:18 +0300

    fixed workflow

--- examples/port-ts/index.ts
+++ examples/port-ts/index.ts
@@ -1,7 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as port from "@port-labs/pulumi";
 
-const entity = new port.Entity("monolith", {
+export const blueprint = new port.Blueprint("microservice", {
+    identifier: "microservice",
+    title: "Microservice",
+    properties: [
+        {
+            identifier: "language",
+            title: "Language",
+            type: "string",
+            required: true,
+        }
+    ]
+});
+
+export const entity = new port.Entity("monolith", {
     identifier: "monolith",
     title: "monolith",
     blueprint: "microservice",
@@ -13,4 +26,3 @@ const entity = new port.Entity("monolith", {
     ]
 });
 
-export const title = entity.title;
